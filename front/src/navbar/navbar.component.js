72538167dfcd43004ad2a33456ebcc8a82542b55
predator1945 <a.szuflicki@gmail.com>
2018-02-10 00:35:26 +0100

    styled navbar

--- front/src/navbar/navbar.component.js
+++ front/src/navbar/navbar.component.js
@@ -7,9 +7,32 @@ export default class Navbar extends Component {
 
         return (
             <div>
-navbar
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <a className="navbar-brand" href="#">RatePal</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-                </div>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">My Profile</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Users</a>
+                            </li>
+                            
+                        </ul>
+                        <form className="form-inline my-2 my-lg-0">
+                            <input className="form-control mr-sm-2" type="search" placeholder="Username" aria-label="Search" />
+                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Log In</button>
+                        </form>
+                    </div>
+                </nav>
+            </div>
         )
     }
 }
